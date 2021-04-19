f2040f9f109fffd8f964243327ce3741c59213c4
Ryan Kelln <ryan.kelln@gmail.com>
2021-04-19 17:24:48 -0400

    Scaling and color to fake 3 dimensions

--- Actors/Crow/Crow.gd
+++ Actors/Crow/Crow.gd
@@ -5,9 +5,16 @@ var _highlight = 0.0
 func _process(_delta):
     look_at(global_position + _velocity)
 
-    if _flock_size and _highlight < _flock_size / 10.0:
-        _highlight += _flock_size / 1000.0
-    elif _highlight > 0:
-        _highlight -= 0.01
+    var r = randf()
+    if r < 0.5:
+        # adjust randomly
+        _highlight -= rand_range(-0.01, 0.01)
+    else:
+        if _flock_size and _highlight < _flock_size / 10.0:
+            _highlight += _flock_size / 1000.0
+        elif _highlight > 0 && r >= 0.3:
+            _highlight -= 0.01
 
-    $Sprite.modulate = Color(0, _highlight, 0)
+    _highlight = clamp(_highlight, 0, 0.3)
+    $Sprite.modulate = Color(_highlight, _highlight, _highlight)
+    scale = Vector2(1.0 - _highlight, 1.0 - _highlight)
