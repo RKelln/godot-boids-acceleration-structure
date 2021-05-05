192184c69334ced7606a8b5e57c03b4f83808e3b
Ryan Kelln <ryan.kelln@gmail.com>
2021-05-05 14:46:37 -0400

    Addd GUI toggle

--- Nodes/GUI.gd
+++ Nodes/GUI.gd
@@ -2,6 +2,8 @@ extends Control
 
 onready var fps := $HBoxContainer/FPS
 
+var _hidden := false
+
 func get_current_values() -> Dictionary:
     return {
         'cohesion' : $HBoxContainer/Cohesion.get_value(),
@@ -16,3 +18,8 @@ func get_current_values() -> Dictionary:
 
 func _process(_delta: float) -> void:
     fps.text = str("FPS:",(Engine.get_frames_per_second()))
+
+func _input(event: InputEvent) -> void:
+    if event.is_action_released("toggle_gui"):
+        _hidden = !_hidden
+        visible = !_hidden
